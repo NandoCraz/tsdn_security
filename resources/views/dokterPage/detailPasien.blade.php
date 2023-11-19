393d30118aada42c684a6a9a98386b63190757c5
Nando <nandodeng75@gamil.com>
2023-11-19 22:38:00 +0700

    nando push 2

--- resources/views/dokterPage/detailPasien.blade.php
+++ resources/views/dokterPage/detailPasien.blade.php
@@ -1,37 +1,42 @@
 @extends('dokterPage.layouts.main')
 @section('content')
     <div class="container">
-        <!-- Your code for the detail pasien section goes here -->
-        <h1>Detail Pasien</h1>
-        <div class="row">
-            <div class="col-md-6 mt-4">
-                <h5>Nama: {{ $pasien->nama }}</h5>
+        <div class="card">
+            <div class="card-header">
+                Detail Pasien
             </div>
-            <div class="col-md-6 mt-4">
-                <h5>Umur: {{ $pasien->usia }}</h5>
-            </div>
-            <div class="col-md-6 mt-4">
-                <h5>Alamat: {{ $pasien->pekerjaan }}</h5>
-            </div>
-            <div class="col-md-6 mt-4">
-                <h5>Jenis Kelamin: {{ $pasien->jenis_kelamin }}</h5>
-            </div>
-            <div class="col-md-6 mt-4">
-                <h5>Penyakit: {{ $pasien->diagnosa }}</h5>
-            </div>
-            <div class="col-md-6 mt-4">
-                <h5>Dokter Menangani: {{ $pasien->nama_dokter }}</h5>
-            </div>
-            <div class="col-md-6 mt-4">
-                <h5>Kota: {{ $pasien->kota }}</h5>
-            </div>
-            <div class="col-md-6 mt-4">
-                <h5>Tanggal Masuk: {{ $pasien->tgl_masuk }}</h5>
-            </div>
-            <div class="col-md-6 mt-4">
-                <h5>Tanggal Keluar: {{ $pasien->tgl_keluar }}</h5>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-md-6 mt-4">
+                        <h5>Nama: {{ $pasien->nama }}</h5>
+                    </div>
+                    <div class="col-md-6 mt-4">
+                        <h5>Umur: {{ $pasien->usia }}</h5>
+                    </div>
+                    <div class="col-md-6 mt-4">
+                        <h5>Kategori: {{ $pasien->pekerjaan }}</h5>
+                    </div>
+                    <div class="col-md-6 mt-4">
+                        <h5>Jenis Kelamin: {{ $pasien->jenis_kelamin }}</h5>
+                    </div>
+                    <div class="col-md-6 mt-4">
+                        <h5>Penyakit: {{ $pasien->diagnosa }}</h5>
+                    </div>
+                    <div class="col-md-6 mt-4">
+                        <h5>Dokter Menangani: {{ $pasien->nama_dokter }}</h5>
+                    </div>
+                    <div class="col-md-6 mt-4">
+                        <h5>Kota: {{ $pasien->kota }}</h5>
+                    </div>
+                    <div class="col-md-6 mt-4">
+                        <h5>Tanggal Masuk: {{ $pasien->tgl_masuk }}</h5>
+                    </div>
+                    <div class="col-md-6 mt-4">
+                        <h5>Tanggal Keluar: {{ $pasien->tgl_keluar }}</h5>
+                    </div>
+                </div>
+                <a href="/data-pasien" class="btn btn-sm btn-secondary mt-5">Kembali</a>
             </div>
         </div>
-        <a href="/data-pasien" class="btn btn-sm btn-secondary mt-5">Kembali</a>
     </div>
 @endsection
